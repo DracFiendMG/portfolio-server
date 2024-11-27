381195034858fd4b2a87fc6a8fde7a3a77a2100c
DracFiendMG <sreeramrdy@gmail.com>
2024-11-27 19:22:30 +0530

    Resolving CORS issue

--- api/projects.js
+++ api/projects.js
@@ -24,6 +24,11 @@ async function getProjects(req, res) {
         const collection = db.collection(collectionName);
 
         const projects = await collection.find({}).toArray();
+
+        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all domains to access this API
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        
         return res.status(200).json(projects);
     } catch (err) {
         return res.status(500).json({ error: err.message });
