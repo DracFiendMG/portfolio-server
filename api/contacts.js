e3ed830873b1a8c562e6341a6ef7bf76cd331297
DracFiendMG <sreeramrdy@gmail.com>
2024-11-30 18:46:01 +0530

    Implemented new API to get list of contact details

--- api/contacts.js
+++ api/contacts.js
@@ -1,24 +1,24 @@
 const connectToCollection = require('../db');
 
-const collectionName = 'content';
+const collectionName = 'contacts';
 
-async function getContent(req, res) {
+async function getContacts(req, res) {
     try {
         const collection = await connectToCollection(collectionName);
-        const content = await collection.find({}).toArray();
+        const contacts = await collection.find({}).toArray();
 
-        if (!content) {
-            return res.status(404).json({ error: "Content not found" });
+        if (!contacts) {
+            return res.status(404).json({ error: "Contacts not found" });
         }
 
         res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all domains to access this API
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
         
-        return res.status(200).json(content);
+        return res.status(200).json(contacts);
     } catch (err) {
         return res.status(500).json({ error: err.message });
     }
 }
 
-module.exports = getContent;
+module.exports = getContacts;
